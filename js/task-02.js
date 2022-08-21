40770b6e9e979fcaf06089804b5bd0ab114605c3
VladosSid <sidorsv121097@gmail.com>
2022-08-21 22:18:11 +0300

    task 02 compleat

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,25 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const firstListEl = document.querySelector("#ingredients");
+console.log(firstListEl);
+
+ingredients.forEach((element) => {
+  const createLiEl = document.createElement("li");
+  console.log(element);
+
+  firstListEl.append(createLiEl);
+
+  createLiEl.textContent = element;
+
+  createLiEl.classList.add("item");
+});
+
+// !!! Не совсем понял как выполнить 4й пункт:
+// "После чего вставит все <li> за одну операцию в список ul#ingredients."
+//  - мой вариант верный???
